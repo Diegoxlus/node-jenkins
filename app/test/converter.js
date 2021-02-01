1298f3f334858ebe051b1835d3a6a0936d9568e7
diegoxlus <lusky1996@gmail.com>
2021-02-02 00:18:49 +0100

    Test Hook

--- app/test/converter.js
+++ app/test/converter.js
@@ -9,6 +9,7 @@ describe("Color Code Converter", function() {
             var greenHex = converter.rgbToHex(0, 255, 0);
             var blueHex  = converter.rgbToHex(0, 0, 255);
 
+
             expect(redHex).to.equal("ff0000");
             expect(greenHex).to.equal("00ff00");
             expect(blueHex).to.equal("0000ff");
